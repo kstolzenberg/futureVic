92ac40b29a9cf342ae715eadf1e816c1e7cfd134
kstolzenberg <kstolzenberg@gmail.com>
2015-08-04 14:26:30 -0700

    + working curve offset

--- curveVic.py
+++ curveVic.py
@@ -6,15 +6,18 @@ import rhinoscriptsyntax as rs
 def curveVic ():
     profiles = []
     firstCrv = rs.GetObject("pick a base curve", 4, True, True)
+    count = rs.GetInteger("how many total curves")
     
-    # note offsetCurve is planar. copy will translate and copy!    
-    # profiles = rs.OffsetCurve(firstCrv, [0,0,10], 2.0)
+    # vertically offset curves
+    for i in range(count):
+        if count == 0 or count == 1:
+            return
+        else:
+            profiles.append(rs.CopyObject(firstCrv,[0,0,5*i]))
+    
+    # loft curves and clean up
+    rs.AddLoftSrf(profiles, closed=True)
     profiles.append(firstCrv)
-    profiles.append(rs.CopyObject(firstCrv,[0,0,5]))
-    print profiles
-    
-    # append curves to a list and then loft result!
-#    rs.AddLoftSrf(profiles)
-#    rs.DeleteObjects(profiles)
+    rs.DeleteObjects(profiles)
 
 curveVic()
