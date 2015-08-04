ec7eb03c4422433d6c1d26974d0bb6c0d42aa5c1
kstolzenberg <kstolzenberg@gmail.com>
2015-08-04 15:33:18 -0700

    + basic funkiness

--- curveVic.py
+++ curveVic.py
@@ -3,7 +3,7 @@
 
 import rhinoscriptsyntax as rs
 
-def curveVic ():
+def curveVic (storyHt):
     profiles = []
     firstCrv = rs.GetObject("pick a base curve", 4, True, True)
     count = rs.GetInteger("how many total curves")
@@ -13,11 +13,19 @@ def curveVic ():
         if count == 0 or count == 1:
             return
         else:
-            profiles.append(rs.CopyObject(firstCrv,[0,0,5*i]))
+            profiles.append(rs.CopyObject(firstCrv,[0,0,storyHt*i]))
+    
+    # funk it up - needs to be more randomized?
+    centerPt = rs.CurveAreaCentroid(profiles[2])
+    rs.RotateObject(profiles[2], centerPt[0], 90)
+    
+    centerPt = rs.CurveAreaCentroid(profiles[3])
+    rs.ScaleObject(profiles[3], centerPt[0], (1.25, 1.25, 0))
     
     # loft curves and clean up
-    rs.AddLoftSrf(profiles, closed=True)
-    profiles.append(firstCrv)
+    finalShape = rs.AddLoftSrf(profiles, closed=False)
+    #profiles.append(firstCrv)
     rs.DeleteObjects(profiles)
+    rs.CapPlanarHoles(finalShape)
 
-curveVic()
+curveVic(15)
